b878296a64acef69b1941b0e1dfa2669972c25e7
lightningRalf <python1234@skiff.com>
2023-04-30 07:58:03 +0200

    + add grouping of dropdown

--- main.js
+++ main.js
@@ -1,36 +1,37 @@
 <script>
-  async function fetchModels() {
-    const response = await fetch('https://huggingface.co/api/models');
-    const data = await response.json();
+    async function fetchModels() {
+      const response = await fetch('https://huggingface.co/api/models');
+      const data = await response.json();
 
-    // Extract relevant model information
-    const models = data.map(model => ({
-      label: model.modelId,
-      value: model.modelId,
-      downloads: model.downloads,
-      createdAt: new Date(model.createdAt)
-    }));
+      // Extract relevant model information
+      const models = data.map(model => ({
+        label: model.modelId,
+        value: model.modelId,
+        downloads: model.downloads,
+        createdAt: new Date(model.createdAt)
+      }));
 
-    // Sort models by downloads and createdAt
-    const topDownloads = models.sort((a, b) => b.downloads - a.downloads).slice(0, 3);
-    const recentUploads = models.sort((a, b) => b.createdAt - a.createdAt).slice(0, 3);
+      // Sort models by downloads and createdAt
+      const topDownloads = models.sort((a, b) => b.downloads - a.downloads).slice(0, 3);
+      const recentUploads = models.sort((a, b) => b.createdAt - a.createdAt).slice(0, 3);
 
-    // Combine and return the final list of models
-    return [...topDownloads, ...recentUploads, ...models];
-  }
+      return { topDownloads, recentUploads, allModels: models };
+    }
 
-  // Fetch model data and populate the models array
-  fetchModels().then(models => {
-    $("#model_name").autocomplete({
-      source: function(request, response) {
-        const results = $.ui.autocomplete.filter(models, request.term);
-        response(results.slice(0, 5));
-      },
-      minLength: 0,
-      delay: 200,
-      autoFocus: true
-    }).on('focus', function() {
-      $(this).autocomplete('search', '');
+    fetchModels().then(({ topDownloads, recentUploads, allModels }) => {
+      populateModelOptions(topDownloads, 'Top Models');
+      populateModelOptions(recentUploads, 'Recent Uploads');
+
+      $("#model_name").autocomplete({
+        source: function (request, response) {
+          const results = $.ui.autocomplete.filter(allModels, request.term);
+          response(results.slice(0, 5));
+        },
+        minLength: 0,
+        delay: 200,
+        autoFocus: true
+      }).on('focus', function () {
+        $(this).autocomplete('search', '');
+      });
     });
-  });
 </script>
