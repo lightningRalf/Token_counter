69afd717b4d5d4e4d9b613f64195c5ec764325a1
lightningRalf <python1234@skiff.com>
2023-04-30 08:45:39 +0200

    + add model dependend tokenizer

--- token_counter.py
+++ token_counter.py
@@ -1,23 +1,24 @@
 import sys
 import re
 import logging
+from transformers import AutoTokenizer
 
-
-def count_tokens(file_path):
+def count_tokens(file_path, model_name='gpt4'):
     try:
         with open(file_path, 'r', encoding='utf-8') as file:
             content = file.read()
-            content = re.sub(r'\!\[.*?\]\(.*?\)|\[(.*?)\]\(.*?\)|\#\#*\s|\`|\*|[\*_]{2,}|[-]{3,}|\d\.\s|\n', '', content)
-            tokens = content.split()
-            token_count = len(tokens)
-            logging.info(f'Token count for {file_path}: {token_count}')
-            return token_count
+        tokenizer = AutoTokenizer.from_pretrained(model_name)
+        tokens = tokenizer.tokenize(content)
+        token_count = len(tokens)
+        logging.info(f'Token count for {file_path}: {token_count}')
+        return token_count
     except FileNotFoundError:
         logging.error(f'File not found: {file_path}')
         print(f'Error: File not found - {file_path}')
     except Exception as e:
         logging.error(f'Error processing file: {file_path} - {str(e)}')
         print(f'Error processing file: {file_path} - {str(e)}')
+
 
 
 if __name__ == "__main__":
